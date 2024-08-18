5cb7b8b4ddc5f2a1ede2b0232420b7e054a5fa96
Cameron Jones <cameron@cameronjonesweb.com.au>
2024-08-18 14:26:18 +1000

    Initial commit

---
+++ fail-5.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * This file should fail PHPCS on 5.X
+ *
+ * @package wpcs-test
+ */
+
+printf(
+	'Hello %s',
+	'World',
+);
---
+++ hello-world.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * This file should always pass
+ *
+ * @package wpcs-test
+ */
+
+echo 'Hello world!';
